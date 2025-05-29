57c97e55abddc1411d6a06c264794fb64e3d91a2
vishvakarma-sumit <sumit.vishwa1999@gmail.com>
2025-05-29 19:44:18 +0530

    added validation

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,4 +2,10 @@ class Recommendation < ApplicationRecord
   belongs_to :user
   belongs_to :book
   has_many :votes
+
+  validates :review, presence: true, length: { maximum: 250 }
+
+  def vote_count
+    votes.count
+  end
 end
