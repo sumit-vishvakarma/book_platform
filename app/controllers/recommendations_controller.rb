c712b522e9ac69341c0c98da1bd08ab3636bbc4c
vishvakarma-sumit <sumit.vishwa1999@gmail.com>
2025-05-29 19:46:20 +0530

    configured recommendation controller

---
+++ app/controllers/recommendations_controller.rb
@@ -0,0 +1,21 @@
+class RecommendationsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @recommendations = Recommendation.includes(:book, :user).trending
+  end
+
+  def create
+    @recommendation = current_user.recommendations.build(recommendation_params)
+    if @recommendation.save
+      redirect_to recommendations_path, notice: "Recommendation created!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def recommendation_params
+    params.require(:recommendation).permit(:book_id, :review)
+  end
+end
